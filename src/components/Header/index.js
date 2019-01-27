0fc3f1887a1ae06ba12a4b6723cdf36dd802eadb
John-Anthony <41270598+JaBautista93@users.noreply.github.com>
2019-01-27 14:58:46 -0600

    fixed app and created new repo

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>DBZ Memory Click Game!</h1>
+      <h2>Click on each image to earn points, but don't click on any more than once per round or the game will reset!</h2>
+    </header>
+  );
+}
+
+export default Header;
